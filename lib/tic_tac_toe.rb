eeffffba2903a7f8f2e3e798af093c9d2be3bc2d
Anne Fausto <a.r.fausto@gmail.com>
2019-10-09 06:52:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,7 +68,7 @@ class TicTacToe
      def won?
      exes = @board.map.with_index  { |position, index| index if position == "X" }
       xxx = WIN_COMBINATIONS.find { |combo| (combo - exes).empty? }
-    return xxx if xxx
+     return xxx if xxx
       owes = @board.map.with_index { |position, index| index if position == "O" }
       ooo = WIN_COMBINATIONS.find { |combo| (combo - owes).empty? }
      return ooo if ooo
